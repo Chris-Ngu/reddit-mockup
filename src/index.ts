58370b8a0acab3aa0b582f0bfe82c9a87ecac19e
Christopher Nguyen <Chris_ngu@pm.me>
2020-08-23 18:39:15 -0500

    connecting to local server

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,23 @@ import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 
+if (!__prod__) {
+    require('dotenv').config();
+}
+
 const main = async () => {
+
     const orm = await MikroORM.init({
-        entities: [Post],
-        dbName: 'redditmock',
-        user: '',
-        password: '',
+        user: process.env.USER,
+        password: process.env.PASSWORD,
         debug: !__prod__,
+        dbName: process.env.NAME,
+    
+        entities: [Post],
         type: 'postgresql'
     });
 
-    const post = orm.em.create(Post, {title: 'my first post'});
+    const post = orm.em.create(Post, { title: 'my first post' });
     //const post = new Post('my first post');
 
     await orm.em.persistAndFlush(post);
