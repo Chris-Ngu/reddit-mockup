7748ac6df2ffa86eea7cfd1f47ea4269e101effc
Christopher Nguyen <Chris_ngu@pm.me>
2020-08-26 19:50:54 -0500

    Resolvers for GraphQL and setting up apollo

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 import microConfig from './mikro-orm.config';
+import { HelloResolver } from './resolver/hello';
+import { PostResolver } from './resolver/post';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
-    const post = orm.em.create(Post, { title: 'my first post' });
-    await orm.em.persistAndFlush(post);
+    const app = express();
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => { em: orm.em }
+    });
 
-    //Showing all posts
-    const posts = await orm.em.find(Post, {});
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log('Backend server started on localhost:4000');
+    });
 
-    console.log(posts);
+    // creating post
+    // const post = orm.em.create(Post, { title: 'my first post' });
+    // await orm.em.persistAndFlush(post);
+
+    // Showing all posts
+    // const records = await orm.em.find(Post, {});
+    // console.log(records);
 }
 
 main().catch((err) => {
